2547438d1e41bd4dda43f3c6cff69ecc05158518
ida4594 <kaha4594@yahoo.co.jp>
2020-10-10 23:23:50 +0900

    8

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -189,3 +189,15 @@ class Scraping
     # ここに処理を書く
   end
 end
+
+class Scraping
+  def self.movie_urls
+    puts 'get movies link URL'
+    # ここに処理を書く
+  end
+
+  def self.get_product(link)
+    puts 'get movie information'
+    # ここに処理を書く
+  end
+end
