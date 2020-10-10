aaa5273debdd80fa2426e0d27f9a41d6b7b890c8
ida4594 <[kaha4594@yahoo.co.jp]>
2020-10-10 22:58:18 +0900

    6

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -93,3 +93,99 @@ class Scraping
     # ここに処理を書く
   end
 end
+
+class Scraping
+  def self.movie_urls
+    puts 'get movies link URL'
+    # ここに処理を書く
+  end
+
+  def self.get_product(link)
+    puts 'get movie information'
+    # ここに処理を書く
+  end
+end
+
+class Scraping
+  def self.movie_urls
+    puts 'get movies link URL'
+    # ここに処理を書く
+  end
+
+  def self.get_product(link)
+    puts 'get movie information'
+    # ここに処理を書く
+  end
+end
+
+class Scraping
+  def self.movie_urls
+    puts 'get movies link URL'
+    # ここに処理を書く
+  end
+
+  def self.get_product(link)
+    puts 'get movie information'
+    # ここに処理を書く
+  end
+end
+
+class Scraping
+  def self.movie_urls
+    puts 'get movies link URL'
+    # ここに処理を書く
+  end
+
+  def self.get_product(link)
+    puts 'get movie information'
+    # ここに処理を書く
+  end
+end
+
+class Scraping
+  def self.movie_urls
+    puts 'get movies link URL'
+    # ここに処理を書く
+  end
+
+  def self.get_product(link)
+    puts 'get movie information'
+    # ここに処理を書く
+  end
+end
+
+class Scraping
+  def self.movie_urls
+    puts 'get movies link URL'
+    # ここに処理を書く
+  end
+
+  def self.get_product(link)
+    puts 'get movie information'
+    # ここに処理を書く
+  end
+end
+
+class Scraping
+  def self.movie_urls
+    puts 'get movies link URL'
+    # ここに処理を書く
+  end
+
+  def self.get_product(link)
+    puts 'get movie information'
+    # ここに処理を書く
+  end
+end
+
+class Scraping
+  def self.movie_urls
+    puts 'get movies link URL'
+    # ここに処理を書く
+  end
+
+  def self.get_product(link)
+    puts 'get movie information'
+    # ここに処理を書く
+  end
+end
