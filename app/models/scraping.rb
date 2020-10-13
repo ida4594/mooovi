1cd42f2c284f0b50eb338087756da4e9810986cd
ida4594 <kaha4594@yahoo.co.jp>
2020-10-13 23:21:05 +0900

    0

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,203 +1,25 @@
 class Scraping
   def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
+    links = []
+    agent = Mechanize.new
+    current_page = agent.get("http://review-movie.herokuapp.com/")
+    elements = current_page.search('.entry-title a')
+    elements.each do |ele|
+      links << ele.get_attribute('href')
+    end
+
+    links.each do |link|
+      get_product('http://review-movie.herokuapp.com/' + link)
+    end
   end
 
   def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
+    agent = Mechanize.new
+    page = agent.get(link)
+    title = page.at('.entry-title').inner_text
+    image_url = page.at('.entry-content img')[:src] if page.at('.entry-content img')
 
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
-  end
-end
-
-class Scraping
-  def self.movie_urls
-    puts 'get movies link URL'
-    # ここに処理を書く
-  end
-
-  def self.get_product(link)
-    puts 'get movie information'
-    # ここに処理を書く
+    product = Product.new(title: title, image_url: image_url)
+    product.save
   end
 end
